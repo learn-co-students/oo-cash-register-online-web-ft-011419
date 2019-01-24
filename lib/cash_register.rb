3920a316cd07a31ae85d4f7c85f508e86cd52471
Kevin Kang <kevinkangg@gmail.com>
2019-01-24 20:12:57 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,17 @@
+class CashRegister
+  attr_accessor :total, :discount
 
+  def initialize(total = 0)
+    @total = 0
+    @discount = total
+  end
+
+  def add_item(item, price, quantity = 1)
+    if quantity == 1
+      @total = price
+    else
+      @total = price * quantity
+    end
+  end
+
+end
